90d1b001efac5de14f357701687cc4d95bc6daea
wenrui-li <66533829+wenrui-li@users.noreply.github.com>
2021-09-09 14:41:05 -0400

    Update createNetwork_random.R

--- R/createNetwork_random.R
+++ R/createNetwork_random.R
@@ -1,7 +1,7 @@
 library(igraph)
 
 # Set a destination for output
-outdir <- "../../Data/RandomNetworks/"
+outdir <- "../Data/RandomNetworks/"
 
 # type 1: ER networks, average degree = 50
 set.seed(1)
@@ -37,8 +37,38 @@ degree4 <- degree(g)
 # degree matrix for 4 networks
 degree_matrix <- cbind(degree1,degree2,degree3,degree4)
  
+
+# type 5: ER networks, average degree = 50, number of nodes = 500
+set.seed(5)
+Nv <- 5e2
+p=50/Nv
+g <- erdos.renyi.game(Nv, p)
+g_edgelist <- get.edgelist(g)
+type5 <- cbind(g_edgelist[,2],g_edgelist[,1])
+
+
+# type 6: ER networks, average degree = 100, number of nodes = 500
+set.seed(6)
+p=100/Nv
+g <- erdos.renyi.game(Nv, p)
+g_edgelist <- get.edgelist(g)
+type6 <- cbind(g_edgelist[,2],g_edgelist[,1])
+
+
+# type 7: scale-free networks, average degree = 50, number of nodes = 500
+set.seed(7)
+g <- barabasi.game(Nv, power = 2, m = 25,directed=FALSE) 
+g_edgelist <- get.edgelist(g)
+type7 <- cbind(g_edgelist[,2],g_edgelist[,1])
+
+
+# type 8: scale-free networks, average degree = 100, number of nodes = 500
+set.seed(8)
+g <- barabasi.game(Nv, power = 2, m = 50,directed=FALSE) 
+g_edgelist <- get.edgelist(g)
+type8 <- cbind(g_edgelist[,2],g_edgelist[,1])
+
+
 # save data
-
-save(type1,type2,type3,type4,degree_matrix,file = paste0(outdir,"randomNetwork.RData"))
-
+save(type1,type2,type3,type4,type5,type6,type7,type8,degree_matrix,file = paste0(outdir,"randomNetwork.RData"))
 
